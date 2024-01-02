e214d6955e274bd9428f26d3e45a6bb2f591f67e
hizqeel <ezekielnizamani0@gmail.com>
2024-01-03 02:24:42 +0500

    learnt  custom type and reciver functions

--- cards/deck.go
+++ cards/deck.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		// card := cardValues[i] + " of " + suit
+		// cards = append(cards, card)
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
