ec4f3c7d58d06f778411c2dcaf8a0518c9981469
hizqeel <ezekielnizamani0@gmail.com>
2024-01-06 01:23:12 +0500

    create file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -9,16 +13,32 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 	for _, suit := range cardSuits {
-		// card := cardValues[i] + " of " + suit
-		// cards = append(cards, card)
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
+
 	}
 	return cards
 }
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 06666)
+}
+func createDeckFromFile(fileName string) deck {
+	bs, error := os.ReadFile(fileName)
+	if error != nil {
+		println("Erorr:", error)
+
+		os.Exit(1)
 	}
 }
